43a67b74f4ec6cbfa7808ec90c13e873a6b368ba
ROM_EU_TTGLOBAL\dpuscas <7x1yy813nx19@gmail.com>
2022-02-25 10:55:00 +0200

    Header and Footer

--- react/confusin/src/components/DishdetailComponent.js
+++ react/confusin/src/components/DishdetailComponent.js
@@ -2,86 +2,85 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardBody, CardText, CardTitle } from 'reactstrap';
 
 
-
-    function RenderComments({comments}) {
-        if (comments != null) {
-            const commentsList = comments.map((comment) => {
-                return (
-                    <li key={comment.id} >
-
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author},
-                            &nbsp;
-                            {new Intl.DateTimeFormat('en-US', {
-                                year: 'numeric',
-                                month: 'short',
-                                day: '2-digit'
-                            }).format(new Date(Date.parse(comment.date)))}
-                        </p>
-                    </li >
-                );
-            });
-
+function RenderComments({ comments }) {
+    if (comments != null) {
+        const commentsList = comments.map((comment) => {
             return (
+                <li key={comment.id} >
 
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {commentsList}
-                    </ul>
-                </div>
-
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author},
+                        &nbsp;
+                        {new Intl.DateTimeFormat('en-US', {
+                            year: 'numeric',
+                            month: 'short',
+                            day: '2-digit'
+                        }).format(new Date(Date.parse(comment.date)))}
+                    </p>
+                </li >
             );
-        }
-
-        else {
-            return (
-                <div>
-
-                </div>
-            );
-        }
-    }
-
-    function RenderDish({dish}) {
-
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-
-                    </CardBody>
-                </Card>
-            );
-        }
-
-        else {
-            return (
-                <div>
-
-                </div>
-            );
-        }
-    }
-
-    const DishDetail = (props) => {
+        });
 
         return (
-            <div className="container">
-                <div className="row">
 
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {commentsList}
+                </ul>
+            </div>
 
-                    <RenderComments comments={props.dish.comments} />
-                </div>
+        );
+    }
+
+    else {
+        return (
+            <div>
+
             </div>
         );
     }
+}
+
+function RenderDish({ dish }) {
+
+    if (dish != null) {
+        return (
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+
+                </CardBody>
+            </Card>
+        );
+    }
+
+    else {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+const DishDetail = (props) => {
+
+    return (
+        <div className="container">
+            <div className="row">
+
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+
+                <RenderComments comments={props.dish.comments} />
+            </div>
+        </div>
+    );
+}
 
 
 
--- react/confusin/src/components/MenuComponent.js
+++ react/confusin/src/components/MenuComponent.js
@@ -19,11 +19,11 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick}/>
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
             </div>
         );
     });
-    
+
     return (
         <div className="container">
             <div className="row">
